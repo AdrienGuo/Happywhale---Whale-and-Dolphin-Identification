babe2b3717cd72cf84fea7ac28e73aa1c8890991
ipvr <ajy1005464@gmail.com>
2022-03-20 16:47:04 +0800

    change NUM_EPOCHS

--- Transformer/train.py
+++ Transformer/train.py
@@ -1,11 +1,11 @@
-from tqdm import tqdm
-
+from matplotlib.pyplot import axis
 import torch
 import torch.nn as nn
 import torch.optim as optim
 from torchvision import transforms
 from torch.cuda.amp import autocast as autocast
 
+from tqdm import tqdm
 from models.transformer import ViT
 from datasets.dataset import get_loaders
 from utils.utils import (check_accuracy)
@@ -13,9 +13,9 @@ from utils.utils import (check_accuracy)
 TRAIN_IMG_DIR = "images/train_images/"
 VAL_IMG_DIR = "images/val_images/"
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
-BATCH_SIZE = 4
+BATCH_SIZE = 32
 LEARNING_RATE = 1e-4
-NUM_EPOCHS = 2
+NUM_EPOCHS = 15
 NUM_WORKERS = 1
 
 train_transform = transforms.Compose(
@@ -28,6 +28,7 @@ train_transform = transforms.Compose(
 
 val_transform = transforms.Compose(
     [
+        transforms.ToPILImage(),
         transforms.Resize((224, 224)),
         transforms.ToTensor(),
     ]
@@ -35,6 +36,7 @@ val_transform = transforms.Compose(
 
 test_transform = transforms.Compose(
     [
+        transforms.ToPILImage(),
         transforms.Resize((224, 224)),
         transforms.ToTensor(),
     ]
@@ -42,22 +44,28 @@ test_transform = transforms.Compose(
 
 def train(loader, model, optimizer, loss_fn, scaler):
     loop = tqdm(loader)
-
+    train_loss = 0
+    num_correct = 0
+    
     for batch_idx, (data, targets) in enumerate(loop):
         data = data.to(device=DEVICE)
         targets = targets.to(device=DEVICE)
-        targets = targets.type(torch.LongTensor)        # PyTorch won't accept a FloatTensor as categorical target
+        # targets = targets.type(torch.LongTensor)        # PyTorch won't accept a FloatTensor as categorical target
 
         # forward
         with torch.cuda.amp.autocast():
-            predictions = model(data)
-            loss = loss_fn(predictions, targets)
+            outputs = model(data)
+            batchLoss = loss_fn(outputs, targets)           # 突然發現怪東西，他這個 outputs 不用取 argmax，那是要怎麼計算阿...
+        train_loss += batchLoss.item()
+        preds = outputs.argmax(axis=-1)
+        num_correct += (preds == targets).sum().item()
         
         # backward
         optimizer.zero_grad()
-        scaler.scale(loss).backward()
+        scaler.scale(batchLoss).backward()
         scaler.step(optimizer)
         scaler.update()
+    print("Train Acc: {:3.6f}, Loss: {:3.6f}".format(num_correct/len(loader)*BATCH_SIZE, train_loss/len(loader)))
 
 
 def main():
@@ -68,7 +76,7 @@ def main():
         batch_size=BATCH_SIZE,
         num_workers=NUM_WORKERS,
     )
-    print(train_loader.dataset.__getitem__(0))
+    print(val_loader.dataset.__getitem__(0))
 
     model = ViT(
         in_channels=3,
